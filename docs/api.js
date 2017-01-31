c74c2b016d630f45167acee2b08c0ba7b5569a77
Eduardo Weiland <eduardo.weiland@quantosobra.com.br>
2017-01-31 11:37:43 -0200

    Publish demo page

---
+++ docs/api.js
@@ -0,0 +1,13 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "SemanticUI.Column",
+        "SemanticUI.Row",
+        "SemanticUI.Table",
+        "SemanticUI.UiTable"
+    ],
+    "modules": [],
+    "allModules": [],
+    "elements": []
+} };
+});
