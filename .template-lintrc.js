7e8228bfcad6618841cf7c4135b93e8f7c0dbb8f
Eduardo Weiland <eduardo.weiland@quantosobra.com.br>
2018-12-03 13:39:36 -0200

    Update ember-cli to 3.5.1

---
+++ .template-lintrc.js
@@ -0,0 +1,10 @@
+'use strict';
+
+module.exports = {
+  extends: 'recommended',
+  rules: {
+    'no-bare-strings': false,
+    'no-invalid-interactive': false,
+    'attribute-indentation': false,
+  }
+};
